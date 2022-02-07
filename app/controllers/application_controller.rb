b67ae3b8e127694e1215866321ef9fb27b1f0ba4
Charles <chall70101@gmail.com>
2022-02-06 20:40:21 -0700

    commit tree b2b6d11b5e2e3894e28a650c0e2eba60fc9ab085

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+    def index
+      plants = Plant.all
+      render json: plants
+    end
+    def show
+      plant = Plant.find_by_(id: params[:id])
+      if plant
+        render json: plant
+      else 
+        render json: {error: "Could not find plant", status: :not_found} 
+      end
+    end
+    def create 
+      plant = Plant.create(plant_params)
+      render json: plant, status: :created
+    end
+    private 
+    def plant_params
+      params.permit(:name, :image, :price)
+    end
 end
